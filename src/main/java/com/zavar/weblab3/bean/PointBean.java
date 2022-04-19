3d069bbdd79117223c3728e72ed67ad1591063d3
Zavar30 <yarusking@mail.ru>
2022-04-19 23:04:56 +0300

    fail

--- src/main/java/com/zavar/weblab3/bean/PointBean.java
+++ src/main/java/com/zavar/weblab3/bean/PointBean.java
@@ -33,7 +33,7 @@ public class PointBean implements Serializable {
             list = (ArrayList<Result>) resultDAO.getAll();
         } catch (PersistenceException e) {
             resultDAO.setConnected(false);
-            resultDAO.close();
+            resultDAO.close();fghgf
             PrimeFaces.current().executeScript("alert('Проблемы с БД, попробуйте позже')");
             list = new ArrayList<Result>();
         }
